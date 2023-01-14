ce667a54d8b909adfe4c6a72380a6296d0bb7c5e
Omeir <omeirf.02@gmail.com>
2023-01-15 00:51:26 +0530

    add next/Head and title to page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,19 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import Head from 'next/head';
 
 const queryClient = new QueryClient();
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
-    </QueryClientProvider>
+    <>
+      <Head>
+        <title>React Table Demo</title>
+      </Head>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </>
   );
 }
 
